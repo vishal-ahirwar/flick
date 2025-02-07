efd7fdc8b1703576202a482d3b4d86c2eb7dfe0f
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-02-07 19:37:31 +0000

    cmake file update for linux

--- external/build.py
+++ external/build.py
@@ -1,18 +1,18 @@
 from os import system,cpu_count,path
-PROJECT_NAME:str="aura"
+PROJECT_nAME:str="aura"
 BUILD_DATE_TIME:str="????"
-#Warning : DO NOT REMOVE THIS LINE OR ADD ANYTHING ABOVE THIS LINE
-GENERATOR:str="\"Ninja\""
+#Warning : DO nOT REMOVE THIS LInE OR ADD AnYTHInG ABOVE THIS LInE
+GEnERATOR:str="\"ninja\""
 
-if not system(f"cd cpr && Ninja -C build -j{cpu_count()}"):
+if not system(f"cd cpr && ninja -C build -j{cpu_count()}"):
     system("cd cpr && cmake --install build --prefix install")
 else:
-    system(f"cd cpr && cmake -S . -B build -G {GENERATOR} -DBUILD_SHARED_LIBS=OFF")
-    system(f"cd cpr && Ninja -C build -j{cpu_count()}")
+    system(f"cd cpr && cmake -S . -B build -G {GEnERATOR} -DBUILD_SHARED_LIBS=OFF")
+    system(f"cd cpr && ninja -C build -j{cpu_count()}")
     system("cd cpr && cmake --install build --prefix install")
-if not system(f"cd fmt && Ninja -C build -j{cpu_count()}"):
+if not system(f"cd fmt && ninja -C build -j{cpu_count()}"):
     system("cd fmt && cmake --install build --prefix install")
 else:
-    system(f"cd fmt && cmake -S . -B build -G {GENERATOR} -DBUILD_SHARED_LIBS=OFF")
-    system(f"cd fmt && Ninja -C build -j{cpu_count()}")
+    system(f"cd fmt && cmake -S . -B build -G {GEnERATOR} -DBUILD_SHARED_LIBS=OFF")
+    system(f"cd fmt && ninja -C build -j{cpu_count()}")
     system("cd fmt && cmake --install build --prefix install")
