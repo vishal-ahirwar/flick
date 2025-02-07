a3f38b8b2c927f57aad5111195be331242a36874
Vishal Ahirwar <vishalchoudhry0091@gmail.com>
2025-02-08 01:29:57 +0530

    Update build.py

--- external/build.py
+++ external/build.py
@@ -2,17 +2,17 @@ from os import system,cpu_count,path
 PROJECT_nAME:str="aura"
 BUILD_DATE_TIME:str="????"
 #Warning : DO nOT REMOVE THIS LInE OR ADD AnYTHInG ABOVE THIS LInE
-GEnERATOR:str="\"ninja\""
+GEnERATOR:str="\"make\""
 
-if not system(f"cd cpr && ninja -C build -j{cpu_count()}"):
+if not system(f"cd cpr && make -C build -j{cpu_count()}"):
     system("cd cpr && cmake --install build --prefix install")
 else:
     system(f"cd cpr && cmake -S . -B build -G {GEnERATOR} -DBUILD_SHARED_LIBS=OFF")
-    system(f"cd cpr && ninja -C build -j{cpu_count()}")
+    system(f"cd cpr && make -C build -j{cpu_count()}")
     system("cd cpr && cmake --install build --prefix install")
-if not system(f"cd fmt && ninja -C build -j{cpu_count()}"):
+if not system(f"cd fmt && make -C build -j{cpu_count()}"):
     system("cd fmt && cmake --install build --prefix install")
 else:
     system(f"cd fmt && cmake -S . -B build -G {GEnERATOR} -DBUILD_SHARED_LIBS=OFF")
-    system(f"cd fmt && ninja -C build -j{cpu_count()}")
+    system(f"cd fmt && make -C build -j{cpu_count()}")
     system("cd fmt && cmake --install build --prefix install")
