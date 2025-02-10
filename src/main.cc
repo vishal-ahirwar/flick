49584b77f216a44998c36ec367ef27bcde337272
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-02-11 01:59:53 +0530

    modified:   src/main.cc

--- src/main.cc
+++ src/main.cc
@@ -2,14 +2,8 @@
 #include <log/log.h>
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
-    {
-        return CLI::showHelp();
-    }
+    if (argc < 2)return CLI::showHelp();
     std::vector<std::string> args{};
-    for (int i = 0; i < argc; ++i)
-    {
-        args.push_back(argv[i]);
-    };
+    for (int i = 0; i < argc; ++i)args.push_back(argv[i]);
     return CLI::createApp(args);
 };
