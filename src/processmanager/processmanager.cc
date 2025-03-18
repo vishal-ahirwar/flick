ecbdd75c1fa930fb29bb89c77a4b71be74d1fae5
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-03-18 20:58:36 +0530

    new file:   src/processmanager/processmanager.cc 	new file:   src/processmanager/processmanager.h

---
+++ src/processmanager/processmanager.cc
@@ -0,0 +1,7 @@
+#include<reproc++/reproc.hpp>
+#include<processmanager/processmanager.h>
+
+int ProcessManager::startProcess(const std::vector<std::string_view> args,bool blog)
+{
+      
+}
---
+++ src/processmanager/processmanager.h
@@ -0,0 +1,10 @@
+#ifndef PROCESS_MANAGER_H
+#define PROCESS_MANAGER_H
+#include <vector>
+#include <string_view>
+class ProcessManager
+{
+public:
+    static int startProcess(const std::vector<std::string_view> args,bool blog=false);
+};
+#endif
